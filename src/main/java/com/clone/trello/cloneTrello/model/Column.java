2b374343d82358ff77ecaba09f7652ef266f2263
pc <pc@MOHIT-PC>
2021-04-25 20:43:32 +0530

    Model designing

--- src/main/java/com/clone/trello/cloneTrello/model/Column.java
+++ src/main/java/com/clone/trello/cloneTrello/model/Column.java
@@ -1,5 +1,13 @@
 package com.clone.trello.cloneTrello.model;
 
+import java.util.List;
+
+import javax.persistence.ManyToMany;
+
 public class Column {
+	
+	@ManyToMany
+	List<Board> boards;
+	
 
 }
--- src/main/java/com/clone/trello/cloneTrello/model/User.java
+++ src/main/java/com/clone/trello/cloneTrello/model/User.java
@@ -1,5 +1,21 @@
 package com.clone.trello.cloneTrello.model;
 
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
+
 public class User {
+	
+	@OneToMany
+	private List<Card> cards;
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
 
 }
