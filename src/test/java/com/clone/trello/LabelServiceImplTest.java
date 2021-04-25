29445a7efbba6a41ec85b6eaccde40221dd60cba
pc <pc@MOHIT-PC>
2021-04-25 20:20:25 +0530

    search based on tag

--- src/test/java/com/clone/trello/LabelServiceImplTest.java
+++ src/test/java/com/clone/trello/LabelServiceImplTest.java
@@ -1,5 +1,24 @@
 package com.clone.trello;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import com.clone.trello.cloneTrello.model.Card;
+import com.clone.trello.cloneTrello.service.LabelService;
+
 public class LabelServiceImplTest {
+	
+	@InjectMocks
+	LabelService labelService;
+	
+	@Test
+	public void fetchAllCardTest() {
+		List<Card> output = null;
+		List<Card> list = labelService.fetchAllCards(null);
+		Assert.assertEquals(list, output);
+	}
 
 }
