751cec2ae3c734cca17ebc83ab11d25e5373619c
pc <pc@MOHIT-PC>
2021-04-25 20:23:00 +0530

    searcg based on Column

--- src/test/java/com/clone/trello/ColumnServiceImplTest.java
+++ src/test/java/com/clone/trello/ColumnServiceImplTest.java
@@ -1,5 +1,24 @@
 package com.clone.trello;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import com.clone.trello.cloneTrello.model.Card;
+import com.clone.trello.cloneTrello.service.ColumnService;
+
 public class ColumnServiceImplTest {
+	
+	@InjectMocks
+	ColumnService columnService;
+	
+	@Test
+	public void fetchAllCard(){
+		List<Card> output = null;
+		List<Card> list = columnService.fetchAllCards(null);
+		Assert.assertEquals(output, list);
+	}
 
 }
